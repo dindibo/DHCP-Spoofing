2b36e790a860eec66a738cfcc63d93cfdfda40bd
Your Name <you@example.com>
2022-01-07 03:26:06 +0000

    WIP: Added parsing and validation

--- dhcp-spoof.py
+++ dhcp-spoof.py
@@ -6,6 +6,9 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 from scapy.all import *
+from sys import argv, exit
+import re
+import socket
 
 # Constants
 
@@ -43,10 +46,50 @@ def is_discover_filter(pack):
 
     return False
 
+def is_mac_valid(x):
+    return bool(re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", x.lower()))
+
+def is_ip_valid(x):
+    try:
+        socket.inet_aton(x)
+        return True
+    except:
+        pass
+    
+    return False
+
+def escape(reason):
+    print(reason)
+    sys.exit(1)
+
+def print_help():
+    print('dhcp-spoof [flags] attacker_mac attacker_ip')
+    print('')
+    print('Flags: ')
+    print('\t-h,  help')
+
+def build_offer_message(discover):
+    pass
+
 # Prints an example discovery
 def handle_DHCP_discover(pack):
     print('Got Discover from ----> ', end='')
     print(pack[Ether].src)
 
-print('Listening...')
-sniff(lfilter=is_discover_filter, prn=handle_DHCP_discover)
+
+def main():
+    if len(sys.argv) != 3 or '-h' in sys.argv:
+        print_help()
+    else:
+        my_mac, my_ip = set(sys.argv[1:])
+
+        if not(is_mac_valid(my_mac) and is_ip_valid(my_ip)):
+            print('Invalid IP or MAC')
+            exit(1)
+
+        print('Listening...')
+        sniff(lfilter=is_discover_filter, prn=handle_DHCP_discover)
+
+
+if __name__ == "__main__":
+    main()
